0ad53ab83d4b3ec33ce75f1ea83e63493cca597c
jaguililla <juanjoaguililla@gmail.com>
2021-03-31 20:47:02 +0200

    Update dependencies

--- build.gradle.kts
+++ build.gradle.kts
@@ -26,7 +26,7 @@ dependencies {
     "implementation"("com.hexagonkt:http_client_ahc:$hexagonVersion")
     "implementation"("com.hexagonkt:serialization_xml:$hexagonVersion")
     "implementation"("com.hexagonkt:serialization_yaml:$hexagonVersion")
-    "implementation"("com.hexagonkt:logging_slf4j:$hexagonVersion")
+    "implementation"("com.hexagonkt:logging_logback:$hexagonVersion")
     "implementation"("com.formdev:flatlaf:$flatlafVersion")
     "implementation"("org.jetbrains.kotlinx:kotlinx-html-jvm:$kotlinxHtmlVersion")
     "implementation"("io.undertow:undertow-servlet:$undertowVersion")
